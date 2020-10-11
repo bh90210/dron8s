2fbecad157c9b62585a498a1ee605936160304d3
bh90210 <ktc@protonmail.com>
2020-10-11 18:15:25 +0300

    k8s test

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/bh90210/dron8s
 
 go 1.15
+
+require (
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
+	k8s.io/api v0.19.2 // indirect
+	k8s.io/apimachinery v0.19.2
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/klog v1.0.0 // indirect
+	k8s.io/utils v0.0.0-20201005171033-6301aaf42dc7 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 	"os"
 	"strings"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
+
+//go:generate docker build  -t bh90210/dron8s:latest -t bh90210/dron8s:$ARG .
+//go:generate docker push bh90210/dron8s:$ARG
+//go:generate docker push bh90210/dron8s:latest
 
 func main() {
 	body := strings.NewReader(
@@ -18,7 +27,39 @@ func main() {
 		body,
 	)
 
-	log.Println(req)
+	fmt.Println(req)
+
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	// get pods in all the namespaces by omitting namespace
+	// Or specify namespace to get pods in particular namespace
+	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+	// Examples for error handling:
+	// - Use helper functions e.g. errors.IsNotFound()
+	// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+	_, err = clientset.CoreV1().Pods("default").Get("example-xxxxx", metav1.GetOptions{})
+	if errors.IsNotFound(err) {
+		fmt.Printf("Pod example-xxxxx not found in default namespace\n")
+	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+		fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+	} else if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Printf("Found example-xxxxx pod in default namespace\n")
+	}
 
 	if err != nil {
 		os.Exit(1)
