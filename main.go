6e8177601a0f302dfefc64074dbaea47eedd3df9
bh90210 <ktc@protonmail.com>
2020-10-19 19:19:07 +0300

    ssa

--- main.go
+++ main.go
@@ -103,6 +103,24 @@ spec:
     spec:
       containers:
       - name: nginx
+		image: nginx:latest
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx-deployment2
+  namespace: default
+spec:
+  selector:
+    matchLabels:
+      app: nginx
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - name: nginx
         image: nginx:latest
 `
 
