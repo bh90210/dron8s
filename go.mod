aa804e81ff2a12d2d4b46396cdef814675b4f07a
bh90210 <ktc@protonmail.com>
2020-10-19 19:14:51 +0300

    ssa

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
 	k8s.io/api v0.19.2 // indirect
 	k8s.io/apimachinery v0.19.2
-	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/client-go v0.0.0-20201007120136-5682372f3538
 	k8s.io/klog v1.0.0 // indirect
 	k8s.io/utils v0.0.0-20201005171033-6301aaf42dc7 // indirect
 )
--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"strings"
 
 	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/discovery"
+	memory "k8s.io/client-go/discovery/cached"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
 )
 
+// GOOS=linux GOARCH=amd64 CGO_ENABLED=0 ARG=v0.0.9 go generate main.go
+//go:generate go build -o dron8s
 //go:generate docker build  -t bh90210/dron8s:latest -t bh90210/dron8s:$ARG .
 //go:generate docker push bh90210/dron8s:$ARG
 //go:generate docker push bh90210/dron8s:latest
@@ -29,6 +41,7 @@ func main() {
 
 	fmt.Println(req)
 
+	fmt.Println("k8s starting")
 	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
@@ -41,7 +54,7 @@ func main() {
 	}
 	// get pods in all the namespaces by omitting namespace
 	// Or specify namespace to get pods in particular namespace
-	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
@@ -50,7 +63,7 @@ func main() {
 	// Examples for error handling:
 	// - Use helper functions e.g. errors.IsNotFound()
 	// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-	_, err = clientset.CoreV1().Pods("default").Get("example-xxxxx", metav1.GetOptions{})
+	_, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
 	if errors.IsNotFound(err) {
 		fmt.Printf("Pod example-xxxxx not found in default namespace\n")
 	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
@@ -61,7 +74,90 @@ func main() {
 		fmt.Printf("Found example-xxxxx pod in default namespace\n")
 	}
 
+	fmt.Println("k8s ended")
+	// if err != nil {
+	// 	os.Exit(1)
+	// }
+
+	//
+	//
+	//
+
+	doSSA(context.Background(), config)
+}
+
+const deploymentYAML = `
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx-deployment
+  namespace: default
+spec:
+  selector:
+    matchLabels:
+      app: nginx
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:latest
+`
+
+var decUnstructured = yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+
+func doSSA(ctx context.Context, cfg *rest.Config) error {
+
+	// 1. Prepare a RESTMapper to find GVR
+	dc, err := discovery.NewDiscoveryClientForConfig(cfg)
 	if err != nil {
-		os.Exit(1)
+		return err
 	}
+	mapper := restmapper.NewDeferredDiscoveryRESTMapper(memory.NewMemCacheClient(dc))
+
+	// 2. Prepare the dynamic client
+	dyn, err := dynamic.NewForConfig(cfg)
+	if err != nil {
+		return err
+	}
+
+	// 3. Decode YAML manifest into unstructured.Unstructured
+	obj := &unstructured.Unstructured{}
+	_, gvk, err := decUnstructured.Decode([]byte(deploymentYAML), nil, obj)
+	if err != nil {
+		return err
+	}
+
+	// 4. Find GVR
+	mapping, err := mapper.RESTMapping(gvk.GroupKind(), gvk.Version)
+	if err != nil {
+		return err
+	}
+
+	// 5. Obtain REST interface for the GVR
+	var dr dynamic.ResourceInterface
+	if mapping.Scope.Name() == meta.RESTScopeNameNamespace {
+		// namespaced resources should specify the namespace
+		dr = dyn.Resource(mapping.Resource).Namespace(obj.GetNamespace())
+	} else {
+		// for cluster-wide resources
+		dr = dyn.Resource(mapping.Resource)
+	}
+
+	// 6. Marshal object into JSON
+	data, err := json.Marshal(obj)
+	if err != nil {
+		return err
+	}
+
+	// 7. Create or Update the object with SSA
+	//     types.ApplyPatchType indicates SSA.
+	//     FieldManager specifies the field owner ID.
+	_, err = dr.Patch(ctx, obj.GetName(), types.ApplyPatchType, data, metav1.PatchOptions{
+		FieldManager: "sample-controller",
+	})
+
+	return err
 }
