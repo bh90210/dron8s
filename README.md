0d47634aa63942d3e8dd943da6418e58cfe0c8fc
bh90210 <ktc@protonmail.com>
2020-10-20 04:22:46 +0300

    code clean up

--- README.md
+++ README.md
@@ -1,26 +1,34 @@
 # Dron8s
 
-Yet another Kubernetes plugin for dynamic [Server Side Apply](https://kubernetes.io/docs/reference/using-api/api-concepts/#server-side-apply). 
+Yet another Kubernetes plugin for Drone using dynamic [Server Side Apply](https://kubernetes.io/docs/reference/using-api/api-concepts/#server-side-apply) to achieve `kubectl apply -f` parity.
 
-explain what dynamic SSA usage means from the perspective of dron8s
-https://ymmt2005.hatenablog.com/entry/2020/04/14/An_example_of_using_dynamic_client_of_k8s.io/client-go#Go-client-libraries
+## Features
+* Creates a resources if it does not exist/updates if it does
+* Can handle multiple yaml configs in one file
+* Can handle most resource types<sup>1</sup>
+* In-cluster/Out-of-cluster usage
 
-NOTE: client-go @ HEAD, resources depent on cluster version/client-go version
+_<sup>1</sup> This plugin use client-go@v0.19.2, while common Kubernetes API will work, some features will not. For more information check the [compatibility matrix](https://github.com/kubernetes/client-go#compatibility-matrix)._
 
 # [in-cluster](https://github.com/kubernetes/client-go/tree/master/examples/in-cluster-client-configuration) use
 
-In-cluster use is intented to only work along [`Kubernetes Runner`](https://docs.drone.io/runner/kubernetes/overview/) with in-cluster deployment scope. That is your pipelines can only create/patch resources within the cluster `Kubernetes Runner` is running.
+In-cluster use is intented to only work along [Kubernetes Runner](https://docs.drone.io/runner/kubernetes/overview/) with in-cluster deployment scope. That is your pipelines can only create/patch resources within the cluster Kubernetes Runner is running.
 
 ## Prerequisites 
+You need to manually create a `clusterrolebinding` to allow cluster resource manipulation from Drone server.
+
+Assuming you installed Drone/Kubernetes Runner using Drone provided Helm charts run:
 ```
-kubectl create clusterrolebinding drone-runner-cluster-admin --clusterrole=cluster-admin --serviceaccount=drone:default
+kubectl create clusterrolebinding dron8s --clusterrole=cluster-admin --serviceaccount=drone:default
 ```
+_If you opted for manual installation you have to replace the `--serviceaccount` flag with the correct service name you used (ie. `--serviceaccount=drone-ci:default`)._
+
 
 ## Example 
 ```
 kind: pipeline
 type: kubernetes
-name: dron8s-example
+name: dron8s-in-cluster-example
 
 
 steps:
@@ -32,10 +40,20 @@ steps:
 
 # [out-of-cluster](https://github.com/kubernetes/client-go/tree/master/examples/out-of-cluster-client-configuration) use
 
+For out-of-cluster use you can choose whichever [runner](https://docs.drone.io/runner/overview/) you prefer but you need to provide you cluster's `kubeconfig` via a secret.
+
+## Prerequisites 
+Create a secret with the contents of kubeconfig
+
+1. gui
+2. kubenrnetes secrets
+3. encrypted
+
+## Example 
 ```
 kind: pipeline
-type: kubernetes
-name: dron8s-example
+type: docker
+name: dron8s-out-of-cluster-example
 
 
 steps:
--- main.go
+++ main.go
@@ -3,26 +3,22 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
 
-	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/discovery"
-
-	// memory "k8s.io/client-go/discovery/cached"
 	"k8s.io/client-go/discovery/cached/memory"
 	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/restmapper"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 // GOOS=linux GOARCH=amd64 CGO_ENABLED=0 ARG=v0.0.9 go generate main.go
@@ -32,86 +28,34 @@ import (
 //go:generate docker push bh90210/dron8s:latest
 
 func main() {
-	// body := strings.NewReader(
-	// 	os.Getenv("PLUGIN_BODY"),
-	// )
+	var config *rest.Config
 
-	// req, err := http.NewRequest(
-	// 	os.Getenv("PLUGIN_METHOD"),
-	// 	os.Getenv("PLUGIN_URL"),
-	// 	body,
-	// )
+	kubeconfig, exists := os.LookupEnv("PLUGIN_KUBECONFIG")
 
-	// fmt.Println(req)
+	switch exists {
+	case true:
+		outOfCluster, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			panic(err.Error())
+		}
+		config = outOfCluster
 
-	fmt.Println("k8s starting")
-	// creates the in-cluster config
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err.Error())
-	}
-	// creates the clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-	// get pods in all the namespaces by omitting namespace
-	// Or specify namespace to get pods in particular namespace
-	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
-	// Examples for error handling:
-	// - Use helper functions e.g. errors.IsNotFound()
-	// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-	_, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
-	if errors.IsNotFound(err) {
-		fmt.Printf("Pod example-xxxxx not found in default namespace\n")
-	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-		fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-	} else if err != nil {
-		panic(err.Error())
-	} else {
-		fmt.Printf("Found example-xxxxx pod in default namespace\n")
+	case false:
+		// create the in-cluster config
+		inCluster, err := rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
+		config = inCluster
 	}
 
-	fmt.Println("k8s ended")
-	// if err != nil {
-	// 	os.Exit(1)
-	// }
-
-	//
-	//
-	//
-
-	er := doSSA(context.Background(), config)
-	log.Println(er)
+	err := ssa(context.Background(), config)
+	log.Println(err)
 }
 
-const deploymentYAML = `
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: nginx-deployment
-  namespace: default
-spec:
-  selector:
-    matchLabels:
-      app: nginx
-  template:
-    metadata:
-      labels:
-        app: nginx
-    spec:
-      containers:
-      - name: nginx
-        image: nginx:latest`
-
-var decUnstructured = yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
-
-func doSSA(ctx context.Context, cfg *rest.Config) error {
+// https://ymmt2005.hatenablog.com/entry/2020/04/14/An_example_of_using_dynamic_client_of_k8s.io/client-go#Go-client-libraries
+func ssa(ctx context.Context, cfg *rest.Config) error {
+	var decUnstructured = yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
 
 	// 1. Prepare a RESTMapper to find GVR
 	dc, err := discovery.NewDiscoveryClientForConfig(cfg)
@@ -126,38 +70,24 @@ func doSSA(ctx context.Context, cfg *rest.Config) error {
 		return err
 	}
 
-	// 	// 2.1.
-	// 	yamlfile, err := os.Open(os.Getenv("PLUGIN_YAML"))
-	// 	defer yamlfile.Close()
-
-	// 	// Splits on newlines by default.
-	// 	scanner := bufio.NewScanner(yamlfile)
-
-	// 	line := 1
-	// 	// https://golang.org/pkg/bufio/#Scanner.Scan
-	// 	for scanner.Scan() {
-	// 		if strings.Contains(scanner.Text(), `
-	// ---
-	// `) {
-	// 			fmt.Println(line)
-	// 		}
-
-	// 		line++
-	// 	}
-
-	// 	if err := scanner.Err(); err != nil {
-	// 		fmt.Println(err)
-	// 	}
-
-	yamlfile2, err := ioutil.ReadFile(os.Getenv("PLUGIN_YAML"))
+	// 2.1. Read user's yaml
+	yaml, err := ioutil.ReadFile(os.Getenv("PLUGIN_YAML"))
 	if err != nil {
 		log.Fatal(err)
 	}
-	text := string(yamlfile2)
 
-	resources := strings.Split(text, "---")
+	// convert it to string
+	text := string(yaml)
+	// Parse each yaml from file
+	configs := strings.Split(text, "---")
+	// Iterate over provided configs
+	for _, v := range configs {
+		// If a yaml starts with `---`
+		// the first slice of `configs` will be empty
+		if len(v) == 0 {
+			continue
+		}
 
-	for _, v := range resources {
 		// 3. Decode YAML manifest into unstructured.Unstructured
 		obj := &unstructured.Unstructured{}
 		_, gvk, err := decUnstructured.Decode([]byte(v), nil, obj)
@@ -196,5 +126,4 @@ func doSSA(ctx context.Context, cfg *rest.Config) error {
 	}
 
 	return err
-
 }
